da7c469ae6d67720e5714260bd4bade84ffb3e2f
James Reeves <jreeves@weavejester.com>
2010-12-03 19:45:50 +0000

    Added autodoc dependency

--- project.clj
+++ project.clj
@@ -1,3 +1,18 @@
 (defproject ring-mock "0.1.0"
-  :description "Library for creating mock Ring request maps"
-  :dependencies [[org.clojure/clojure "1.2.0"]])
+  :description "A library for creating mock Ring request maps"
+  :dependencies [[org.clojure/clojure "1.2.0"]]
+  :dev-dependencies
+    [[org.clojure/clojure-contrib "1.2.0"]
+     [autodoc "0.7.1" :exclusions [org.clojure/clojure
+                                   org.clojure/clojure-contrib]]]
+  :autodoc
+    {:name "Ring-Mock"
+     :description "A library for creating mock Ring request maps"
+     :copyright "Copyright 2010 James Reeves"
+     :root "."
+     :source-path "src"
+     :web-src-dir "http://github.com/weavejester/ring-mock/blob/"
+     :web-home "http://weavejester.github.com/ring-mock"
+     :output-path "autodoc"
+     :namespaces-to-document ["ring.mock"]
+     :load-except-list [#"/test/" #"project.clj"]})
