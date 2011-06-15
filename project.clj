75b01bb60117e5230d0cea235be19841e22f4104
Ryan Senior <senior.ryan@gmail.com>
2011-06-15 13:09:58 -0500

    Bumped version to 0.1.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ring-mock "0.1.1"
+(defproject ring-mock "0.1.2-SNAPSHOT"
   :description "A library for creating mock Ring request maps"
   :dependencies [[org.clojure/clojure "1.2.0"]]
   :dev-dependencies
