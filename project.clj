35892c4d29ec7e54d523537deaca0ef71e8e8bbf
James Reeves <jreeves@weavejester.com>
2012-04-21 13:05:29 +0100

    Removed clojure-contrib dev-dependency

--- project.clj
+++ project.clj
@@ -1,5 +1,3 @@
 (defproject ring-mock "0.1.1"
   :description "A library for creating mock Ring request maps"
-  :dependencies [[org.clojure/clojure "1.2.0"]]
-  :dev-dependencies
-    [[org.clojure/clojure-contrib "1.2.0"]])
+  :dependencies [[org.clojure/clojure "1.2.0"]])
