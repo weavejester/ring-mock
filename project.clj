6faacbe4a68dd4249ad1b1d2522eb54f66fd42d9
James Reeves <jreeves@weavejester.com>
2012-04-21 13:05:58 +0100

    Updated minimum Clojure version to 1.2.1

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject ring-mock "0.1.1"
   :description "A library for creating mock Ring request maps"
-  :dependencies [[org.clojure/clojure "1.2.0"]])
+  :dependencies [[org.clojure/clojure "1.2.1"]])
