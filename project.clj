eceda34fc44003e9c5346b2c13a3ed92282e94d0
James Reeves <jreeves@weavejester.com>
2012-04-21 13:20:00 +0100

    Added Codox as a plugin

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject ring-mock "0.1.1"
   :description "A library for creating mock Ring request maps"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :plugins [[codox "0.6.1"]])
