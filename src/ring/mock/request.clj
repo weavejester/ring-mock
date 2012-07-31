4ae0a0adfaa61a40f44e9cbf1dd716add5998f59
Kevin Neaton <neatonk@gmail.com>
2012-07-31 10:46:53 -0500

    Handle request params differently for :get and :post methods.
    As a shortcut, params provided for a :get will be appended to the
    :query-string and params provided for a :post will be used as the
    request :body.
    
    * moved request down to make 'body and 'query-string available
    * added new 'query-string fn for modifying a request.
    * preserves existing :query-string when using 'query-string.
    * replace old 'query-string with simpler 'append-query.
    * added test cases for 'adding params in :post'

--- src/ring/mock/request.clj
+++ src/ring/mock/request.clj
@@ -13,38 +13,13 @@
       (str (URLEncoder/encode (name k)) "="
            (URLEncoder/encode (str v))))))
 
-(defn- query-string
-  "Create a query string from a URI and a map of parameters."
-  [uri params]
-  (let [query (.getRawQuery uri)]
-    (if (or query params)
-      (string/join "&"
-        (remove string/blank?
-                [query (encode-params params)])))))
-
-(defn request
-  "Create a minimal valid request map from a HTTP method keyword, a string
-  containing a URI, and an optional map of parameters that will be added to
-  the query string of the URI. The URI can be relative or absolute. Relative
-  URIs are assumed to go to http://localhost."
-  ([method uri]
-     (request method uri nil))
-  ([method uri params]
-     (let [uri    (URI. uri)
-           host   (or (.getHost uri) "localhost")
-           port   (if (not= (.getPort uri) -1) (.getPort uri))
-           scheme (.getScheme uri)
-           path   (.getRawPath uri)]
-       {:server-port    (or port 80)
-        :server-name    host
-        :remote-addr    "localhost"
-        :uri            (if (string/blank? path) "/" path)
-        :query-string   (query-string uri params)
-        :scheme         (or (keyword scheme) :http)
-        :request-method method
-        :headers        {"host" (if port
-                                  (str host ":" port)
-                                  host)}})))
+(defn- append-query
+  "Encode and append a map of parameters to a url query string."
+  [query params]
+  (if (or query params)
+    (string/join "&"
+      (remove string/blank?
+              [query (encode-params params)]))))
 
 (defn header
   "Add a HTTP header to the request map."
@@ -66,6 +41,11 @@
       (assoc :content-length length)
       (header :content-length length)))
 
+(defn query-string
+  "Add a map of parameters to the query string of the request."
+  [request params]
+  (update-in request [:query-string] #(append-query % params)))
+
 (defmulti body
   "Set the body of the request. The supplied body value can be a string or
   a map of parameters to be url-encoded."
@@ -84,3 +64,34 @@
   (-> request
       (content-type "application/x-www-form-urlencoded")
       (body (encode-params params))))
+
+(defn request
+  "Create a minimal valid request map from a HTTP method keyword, a string
+  containing a URI, and an optional map of parameters that will be added to
+  the query string of the URI. The URI can be relative or absolute. Relative
+  URIs are assumed to go to http://localhost."
+  ([method uri]
+     (request method uri nil))
+  ([method uri params]
+     (let [uri    (URI. uri)
+           host   (or (.getHost uri) "localhost")
+           port   (if (not= (.getPort uri) -1) (.getPort uri))
+           scheme (.getScheme uri)
+           path   (.getRawPath uri)
+           query  (.getRawQuery uri)
+           req    {:server-port    (or port 80)
+                   :server-name    host
+                   :remote-addr    "localhost"
+                   :uri            (if (string/blank? path) "/" path)
+                   :query-string   query
+                   :scheme         (or (keyword scheme) :http)
+                   :request-method method
+                   :headers        {"host" (if port
+                                             (str host ":" port)
+                                             host)}}]
+       (if params
+         (case method
+           :get  (query-string req params)
+           :post (body req params)
+           req)
+         req))))
