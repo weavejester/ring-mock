aaf5591ec642438027aed4b05a30cbfe3a98aa03
James Reeves <jreeves@weavejester.com>
2012-08-08 01:21:43 +0100

    Released 0.1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ring-mock "0.1.2"
+(defproject ring-mock "0.1.3"
   :description "A library for creating mock Ring request maps"
   :dependencies [[org.clojure/clojure "1.2.1"]]
   :plugins [[codox "0.6.1"]])
