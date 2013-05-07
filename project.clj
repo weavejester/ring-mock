972eee659a7f6f47156c050d83dd596064a03f1f
James Reeves <jreeves@weavejester.com>
2013-05-07 20:28:01 +0100

    Updated Codox to 0.6.4

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "A library for creating mock Ring request maps"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [ring/ring-codec "1.0.0"]]
-  :plugins [[codox "0.6.1"]]
+  :plugins [[codox "0.6.4"]]
   :profiles {
     :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
     :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
